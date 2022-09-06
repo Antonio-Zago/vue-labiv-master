12cd8d40c29f7e823617e83a0f9f66237a0a2bde
Antonio Zago <antoniozagodev@gmail.com>
2022-09-06 20:51:50 -0300

    Aula 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link> | <!--Serve para links internos onde não é necessario recarregar a pagina-->
+      <router-link to="/about">About</router-link> |
+      <router-link to="/usuario">Usuario</router-link>
     </nav>
-    <router-view/>
+    <router-view/><!--Espaço onde será montado a página-->
   </div>
 </template>
 
---
+++ src/views/UsuarioView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="usuario">
+    <p>Palmeiras não tem mundial</p>
+    <p v-if="nome.length > 5">{{nome}}</p>
+    <p><input type="text" v-model="nome"></p>
+    <ul >
+      <li v-for="(anotacao,i) in anotacoes " :key="i">{{anotacao}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default{
+  name:'UsuarioView',
+  data(){
+    return {
+      nome: 'teste',
+      anotacoes:['oi', 'tchau', 'adfa']
+    }
+  }
+}
+</script>
